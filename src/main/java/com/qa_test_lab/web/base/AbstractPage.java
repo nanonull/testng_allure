4741bd199e39e338a1590696aa4cee9d849fc683
user <dmitriy.ltv@gmail.com>
2018-10-29 12:54:38 +0200

    add findBy init

--- src/main/java/com/qa_test_lab/web/base/AbstractPage.java
+++ src/main/java/com/qa_test_lab/web/base/AbstractPage.java
@@ -1,15 +1,16 @@
 package com.qa_test_lab.web.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public abstract class AbstractPage {
     protected final WebDriver driver;
     protected final String baseUrl;
 
     public AbstractPage(WebDriver driver, String baseUrl) {
-
         this.driver = driver;
         this.baseUrl = baseUrl;
+        PageFactory.initElements(driver, this);
     }
 
     public void open() {
--- src/main/java/com/qa_test_lab/web/base/AbstractPanel.java
+++ src/main/java/com/qa_test_lab/web/base/AbstractPanel.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public abstract class AbstractPanel {
     protected final WebDriver driver;
@@ -12,6 +13,7 @@ public abstract class AbstractPanel {
     public AbstractPanel(WebDriver driver, By baseLocator) {
         this.driver = driver;
         this.baseLocator = baseLocator;
+        PageFactory.initElements(driver, this);
     }
 
     public WebElement findRootElement() {
